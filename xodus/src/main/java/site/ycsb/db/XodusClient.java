5b6cbaf312841b127b2b92c0ae0111200aa43fad
Lesslyrics <alina_boshenko@mail.ru>
2023-06-12 17:59:13 +0200

    updates to  xodus client

--- xodus/src/main/java/site/ycsb/db/XodusClient.java
+++ xodus/src/main/java/site/ycsb/db/XodusClient.java
@@ -5,6 +5,7 @@ import jetbrains.exodus.bindings.StringBinding;
 import jetbrains.exodus.env.*;
 import site.ycsb.*;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -12,16 +13,29 @@ import java.util.Vector;
 
 public class XodusClient extends DB {
 
-  Environment env = null;
-  private String STORE_NAME = "MyStore";
+  private Environment env;
+  private Store store;
 
+  private static final String DB_NAME = "ycsb";
+  private static final String DB_PATH = "/home/me/.myAppData";
+  private static final String STORE_NAME = "store";
+
+  @Override
   public void init() throws DBException {
-    env = Environments.newInstance("/home/me/.myAppData");
+    if (DB_PATH == null) {
+      throw new DBException("Database path property 'xodus.dbpath' is missing.");
+    }
+
+    env = Environments.newInstance(DB_PATH);
+    store = env.computeInTransaction(txn -> env.openStore(STORE_NAME, StoreConfig.WITHOUT_DUPLICATES, txn));
   }
+
 
   @Override
   public void cleanup() throws DBException {
-
+    // todo: complete delete?
+    store.close();
+    env.close();
   }
 
 
@@ -32,7 +46,7 @@ public class XodusClient extends DB {
     env.executeInTransaction(txn -> {
       final Store store = env.openStore(STORE_NAME, StoreConfig.WITHOUT_DUPLICATES, txn);
 
-      for (Map.Entry<String, ByteIterator> value: values.entrySet()){
+      for (Map.Entry<String, ByteIterator> value : values.entrySet()) {
         store.put(txn, StringBinding.stringToEntry(value.getKey()),
             StringBinding.stringToEntry(byteIteratorToString(value.getValue())));
       }
@@ -44,33 +58,34 @@ public class XodusClient extends DB {
     return new String(byteIter.toArray());
   }
 
+  // todo fix returns
   @Override
   public Status delete(String table, String key) {
     env.executeInTransaction(txn -> {
       final Store store = env.openStore(STORE_NAME, StoreConfig.WITHOUT_DUPLICATES, txn);
-        store.delete(txn, StringBinding.stringToEntry(key));
+      store.delete(txn, StringBinding.stringToEntry(key));
     });
     return Status.OK;
-
   }
 
   // TODO: not sure ab this: key = The record key of the record to insert.
   @Override
   public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+    ByteIterable value = null;
     env.executeInTransaction(txn -> {
+      // todo
       final Store store = env.openStore(STORE_NAME, StoreConfig.WITHOUT_DUPLICATES, txn);
-      final ByteIterable value = store.get(txn, StringBinding.stringToEntry(key));
+//      value = store.get(txn, StringBinding.stringToEntry(key));
     });
     env.close();
-    return Status.OK;
+    return value != null ? Status.OK : Status.ERROR;
 
   }
 
   @Override
   public Status update(String table, String key, Map<String, ByteIterator> values) {
-    // todo not yet implemented
-    return Status.OK;
-
+    // todo ?
+    return insert(table, key, values);
   }
 
   @Override
