2a6dcfc29f4fac5a58d48f2e5e87a8e675776fcc
Lesslyrics <alina.boshchenko@jetbrains.com>
2023-07-06 18:37:43 +0200

    benchmarking for the new OL concept

--- xodus/src/main/java/site/ycsb/db/XodusClient.java
+++ xodus/src/main/java/site/ycsb/db/XodusClient.java
@@ -2,47 +2,56 @@ package site.ycsb.db;
 
 import jetbrains.exodus.ByteIterable;
 import jetbrains.exodus.bindings.StringBinding;
-import jetbrains.exodus.env.*;
+import jetbrains.exodus.newLogConcept.MVCC.MVCCDataStructure;
+import jetbrains.exodus.newLogConcept.transaction.Transaction;
 import site.ycsb.*;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Client class for the Xodus DB.
  **/
 public class XodusClient extends DB {
 
-  private Environment env;
-  private Store store;
+//  private Environment env;
+//  private Store store;
 
   private static final String DB_PATH = "/home/alinaboshchenko/.myAppData";
   private static final String STORE_NAME = "store";
+  private MVCCDataStructure mvccComponent = new MVCCDataStructure();
 
   @Override
   public void init() {
-    env = Environments.newInstance(DB_PATH);
-    store = env.computeInTransaction(txn -> env.openStore(STORE_NAME, StoreConfig.WITHOUT_DUPLICATES, txn));
+//    env = Environments.newInstance(DB_PATH);
+//    store = env.computeInTransaction(txn -> env.openStore(STORE_NAME, StoreConfig.WITHOUT_DUPLICATES, txn));
   }
 
   @Override
   public void cleanup() {
     // todo: complete delete?
     // store.close();
-    env.close();
+//    env.close();
   }
 
   // TODO: not sure ab this: key = The record key of the record to insert.
   @Override
   public Status insert(String table, String key, Map<String, ByteIterator> values) {
-    env.executeInTransaction(txn -> {
-        for (Map.Entry<String, ByteIterator> value : values.entrySet()) {
-          store.put(txn, StringBinding.stringToEntry(value.getKey()),
-              StringBinding.stringToEntry(byteIteratorToString(value.getValue())));
-        }
-      });
+    Transaction writeTransaction = mvccComponent.startWriteTransaction();
+
+    for (Map.Entry<String, ByteIterator> value : values.entrySet()) {
+      mvccComponent.put(writeTransaction, StringBinding.stringToEntry(value.getKey()),
+          StringBinding.stringToEntry(byteIteratorToString(value.getValue())));
+    }
+    try {
+      mvccComponent.commitTransaction(writeTransaction);
+    } catch (ExecutionException e){
+      System.out.println(e);
+    } catch (InterruptedException e){
+      System.out.println(e);
+    }
     return Status.OK;
   }
 
@@ -53,19 +62,19 @@ public class XodusClient extends DB {
   // todo fix returns
   @Override
   public Status delete(String table, String key) {
-    env.executeInTransaction(txn -> store.delete(txn, StringBinding.stringToEntry(key)));
+    Transaction writeTransaction = mvccComponent.startWriteTransaction();
+    mvccComponent.remove(writeTransaction, StringBinding.stringToEntry(key), null);
+
     return Status.OK;
   }
 
   // TODO: not sure ab this: key = The record key of the record to insert.
   @Override
   public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
-    env.executeInTransaction(txn -> {
-      // todo
-        ByteIterable value = store.get(txn, StringBinding.stringToEntry(key));
-        assert value != null;
-      });
-    env.close();
+    Transaction readTransaction = mvccComponent.startReadTransaction();
+    ByteIterable record = mvccComponent.read(readTransaction,
+        StringBinding.stringToEntry(key));
+    assert record != null;
     return Status.OK;
   }
 
